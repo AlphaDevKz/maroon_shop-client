c72e84b316eb98cd71467a75fbeeae4a793f66b6
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2023-07-22 07:45:20 +0600

    page/main add section
    added history section

--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -3,6 +3,7 @@ import { Welcome } from './welcome/Welcome'
 import { Bestseller } from './bestselller'
 import { Banner } from './banner/Banner'
 import { Offer } from './offer/Offer'
+import { History } from './history/History'
 
 export const Main = () => {
    return (
@@ -11,6 +12,7 @@ export const Main = () => {
          <Bestseller />
          <Banner mode='more' />
          <Offer />
+         <History />
       </section>
    )
 }
